75d5be59a4dc9a05f39bf59402e3db1ca2040ca4
rodrigoosoares <rodrigo.o.soares@hotmail.com>
2018-09-08 13:46:13 -0300

    MainPage Presenter e remoção de res que não serão utilizados

--- app/src/main/java/com/project/rodrigo/yourrpg/activities/MainActivity.java
+++ app/src/main/java/com/project/rodrigo/yourrpg/activities/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -28,9 +27,10 @@ public class MainActivity extends AppCompatActivity
     private ImageView ivImageJogador;
     private ProgressBar pbAtualJogadorXp;
     private ListView listaMissoes;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
 
     private MainPagePresenter mPresenter;
-    private Intent navigationIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mPresenter = new MainPagePresenter(this);
+        mPresenter = new MainPagePresenter(this, this);
 
         tvNomeJogador = findViewById(R.id.tvNomeJogador);
         tvClasseJogador = findViewById(R.id.tvClasseJogador);
@@ -47,18 +47,13 @@ public class MainActivity extends AppCompatActivity
         ivImageJogador = findViewById(R.id.ivImageJogador);
         pbAtualJogadorXp = findViewById(R.id.pbAtualJogadorXp);
         listaMissoes = findViewById(R.id.lvMainMissoes);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
         mPresenter.createToolbarUser(tvNomeJogador,tvClasseJogador,tvNivelJogador,tvStatusXp,ivImageJogador,pbAtualJogadorXp);
         mPresenter.createListaMissoes(listaMissoes);
+        mPresenter.createNavigationDrawer(drawer, toolbar);
 
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
@@ -81,12 +76,8 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -97,26 +88,6 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_tutorial) {
-            navigationIntent = new Intent(this, TutorialActivity.class);
-            startActivity(navigationIntent);
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+        return mPresenter.navigationItemSelected(item);
     }
 }
--- app/src/main/java/com/project/rodrigo/yourrpg/presenters/MainPagePresenter.java
+++ app/src/main/java/com/project/rodrigo/yourrpg/presenters/MainPagePresenter.java
@@ -1,11 +1,20 @@
 package com.project.rodrigo.yourrpg.presenters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.project.rodrigo.yourrpg.R;
+import com.project.rodrigo.yourrpg.activities.MainActivity;
+import com.project.rodrigo.yourrpg.activities.TutorialActivity;
 import com.project.rodrigo.yourrpg.helpers.MissoesListAdapter;
 import com.project.rodrigo.yourrpg.helpers.SharedPreferencesHelper;
 import com.project.rodrigo.yourrpg.models.Jogador;
@@ -16,11 +25,15 @@ import java.util.ArrayList;
 public class MainPagePresenter {
 
     private Context context;
+    private MainActivity activity;
     private Jogador jogadorPrefs;
+    private Intent navigationIntent;
     private SharedPreferencesHelper sharedPreferences;
 
-    public MainPagePresenter(Context context) {
+    public MainPagePresenter(Context context, MainActivity activity) {
+
         this.context = context;
+        this.activity = activity;
     }
 
     public void createToolbarUser(TextView tvNome, TextView tvClasse, TextView tvNivel, TextView tvStatusXp, ImageView ivImagem, ProgressBar pbLevel){
@@ -41,6 +54,24 @@ public class MainPagePresenter {
         MissoesListAdapter missoesListAdapter = new MissoesListAdapter(context, arrayList);
 
         listaMissoes.setAdapter(missoesListAdapter);
+    }
+    public void createNavigationDrawer(DrawerLayout drawer, Toolbar toolbar){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                activity, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+    public boolean navigationItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.nav_tutorial) {
+            navigationIntent = new Intent(context, TutorialActivity.class);
+            activity.startActivity(navigationIntent);
+        }
+
+        DrawerLayout drawer = activity.findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 
